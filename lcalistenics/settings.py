e923e7d3b753308bd1e2d8fd25f55b18419409ae
Esteban Alonzo <pstban10@gmail.com>
2023-11-29 10:26:28 -0300

    cambiamos de sqlite3 a mysql, para trabajar con varias tablas

--- lcalistenics/settings.py
+++ lcalistenics/settings.py
@@ -90,11 +90,14 @@ WSGI_APPLICATION = 'lcalistenics.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'Django_project',
+        'USER': 'root',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'PASSWORD': '0987654321',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
