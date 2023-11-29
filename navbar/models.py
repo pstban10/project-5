952e44bb40387564b874232ce9b78216a9c45b4c
Esteban Alonzo <pstban10@gmail.com>
2023-11-29 14:34:14 -0300

    pasamos de usar sqlite3 a MYSQL

--- navbar/models.py
+++ navbar/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class PatronMovimiento(models.Model):
+    nombre = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.nombre
+
+
+class EjercicioCalistenia(models.Model):
+    nombre = models.CharField(max_length=255)
+    dificultad = models.IntegerField()
+    musculos = models.CharField(max_length=255)
+    patron = models.ForeignKey(PatronMovimiento, on_delete=models.RESTRICT)
+
+    def __str__(self):
+        return self.nombre
+
+
+class Rutina(models.Model):
+    nombre = models.CharField(max_length=255)
+    dificultad = models.IntegerField()
+    Ejercicios = models.ManyToManyField(EjercicioCalistenia)
+    creador = models.ForeignKey(User, on_delete=models.RESTRICT)
+    creada_el = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.nombre
